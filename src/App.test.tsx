62431b8e078bf9a89695ad44c6bc20c50255cfa2
k-nakamura <k-nakamura@gmail.com>
2020-11-02 12:43:53 +0900

    setup for react-router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from "./view/Home";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('render home', () => {
+  render(<Home/>);
+  const linkElement = screen.getByText(/This is Home/i);
   expect(linkElement).toBeInTheDocument();
-});
+})
