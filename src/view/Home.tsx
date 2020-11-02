8a60a5388d2133ecaf4fd5e61aa763a01f088424
k-nakamura <k-nakamura@gmail.com>
2020-11-02 16:57:32 +0900

    set up theming

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Card, Icon} from "semantic-ui-react";
 
 function Home() {
-  return(
+  const [count, setCount] = useState(0);
+  const handlePlus = () => setCount(count + 1);
+  const handleMinus = () => setCount(count - 1);
+
+  return (
     <div>
       This is Home.
+      <Card>
+        <Card.Header textAlign={"left"}>
+          Count
+        </Card.Header>
+        <Card.Meta textAlign={"left"}>
+          count up/down
+        </Card.Meta>
+        <Card.Content>
+          <Card.Description>
+            <Button.Group>
+              <Button icon onClick={handlePlus}>
+                <Icon name={'plus'}/>
+              </Button>
+              <Button.Or text={count}/>
+              <Button icon onClick={handleMinus}>
+                <Icon name={'minus'}/>
+              </Button>
+            </Button.Group>
+          </Card.Description>
+          <Card.Description>
+          </Card.Description>
+        </Card.Content>
+      </Card>
     </div>
   )
 }
